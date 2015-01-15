424418b8cc5cad78ae742c7c3e3fc01909389847
Coen de Jong <co.dejong@gmail.com>
2015-01-16 00:43:59 +0100

    adding chords on the lines and an animation test

--- js/main.js
+++ js/main.js
@@ -26,3 +26,43 @@ function start_song(){
      	setTimeout("$(document).off('keypress')", chordTime*1000 + 100);
     }
 }
+
+$(function()
+	{
+		console.log('test');
+		$('#button-start').on('click', startGame);
+
+		//populate the game
+		var i = 0;
+		var j = 1;
+		for(i = 0; i < 10; i++)
+		{
+			var oldChord = $('.chord.cord-'+j).last();
+			var newChord = oldChord.clone();
+			newChord.css({top: (parseInt(oldChord.css('top').split('px')[0]) + 1000 + 'px') });
+			$('#chords').append(newChord);
+
+			console.log(oldChord.css('top'), newChord);
+
+			j++;
+
+			if(j > 4)
+				j = 1;
+		}
+});
+
+function startGame(event)
+{
+	console.log('starting game...');
+	var songDuration = 10; //in seconds
+	var speed = 1; //pixels per millisecond
+	document.getElementById('chords').style.top = '0px';
+	var interval = window.setInterval(function()
+	{
+		document.getElementById('chords').style.top = parseInt(document.getElementById('chords').style.top.split('px')[0]) - 1 + 'px';
+		//$('#chords').css({'top': '-=1px'});
+	}, 1);
+
+	window.setTimeout(function() { window.clearInterval(interval) }, (songDuration * 1000))
+
+}
