d45818619bde285480a59e803fad4c99b48780f3
Henrique <lima.henrique@gmail.com>
2015-01-16 11:31:27 +0000

    final

--- js/events.js
+++ js/events.js
@@ -1,10 +1,34 @@
-function match_event(userChord, tabChord){
+function setupTimersForGame()
+{
+  var totalNumChords = $('.chord').nextAll().length;
+
+  $('.chord').each(function(i, chord)
+  {
+  	timertime = ($(chord).data('timing')*1000) - 200 + 500;
+  	key = $(chord).data('key');
+
+  	console.log(key, timertime);
+	  setTimeout("$(document).on('keypress', function(e){ match_event('" + $(chord).attr('id') + "', String.fromCharCode(e.which) ,'"  
+	 	  + key + "');}); console.log('timer for "+key+" begins after "+timertime+" ms');", timertime);
+
+	  setTimeout("$(document).off('keypress')", ($(chord).data('timing')*1000) + 200 + 500);
+
+	  if(!--totalNumChords)
+	  	$(document).trigger('timersready');
+
+  });
+
+
+}
+function match_event(elemId, userChord, tabChord){
 	
+	console.log('Lets see if the user pressed the right key: '+userChord, tabChord);
 	if(userChord.toLowerCase() == tabChord.substring(0,1).toLowerCase()){
-		alert("CHORD " + userChord + " MATCH!")
+		$("#" + elemId).addClass('exploded');
+		console.log("CHORD " + userChord + " MATCH!")
 	}
 	else{
-		alert("CHORDS " + userChord + " AND " + tabChord + " DON'T MATCH!")
+		console.log("CHORDS " + userChord + " AND " + tabChord + " DON'T MATCH!")
 	}
 }
 
--- js/main.js
+++ js/main.js
@@ -15,15 +15,31 @@ $(document).ready(function() {
       start_song();
    	});
 
+  $('#button-start').on('click', startGame);
+
+   soundcloud.addEventListener('onPlayerReady', function(scPlayer, data) {
+      //scPlayer.api_play();
+      $('#button-start').show();
+   });
+  //SC.oEmbed("https://soundcloud.com/user723751824/get-lucky-daft-punk-feat", {color: "ff0066"},  document.getElementById("putTheWidgetHere"));
+  //console.log(player);
+
 });
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '250',
-    width: '500',
-    videoId: 'bBJ9RnbK8G4',
-  });
-}
+//function onYouTubeIframeAPIReady() {
+//  player = new YT.Player('player', {
+//    height: '250',
+//    width: '500',
+//    videoId: 'bBJ9RnbK8G4',
+//    events: {
+//      'onReady': onPlayerReady
+//    }
+//  });
+//}
+
+function onPlayerReady(event) {
+  $("#button-start").show();
+} 
 
 function start_song(){
 	  
@@ -41,27 +57,36 @@ function start_song(){
 
       //setTimeout(function(){matchTime = true;}, chordTime*1000 -100);
       //setTimeout(function(){matchTime = false;}, chordTime*1000 + 100);
-      draw_chord(chordTime, chordNotation);
       
-      setTimeout("$(document).on('keypress', function(e){ match_event(String.fromCharCode(e.which) ,'"  
-     	  + chordNotation + "');});", chordTime*1000 - 100);
+      //if(chordParsed[0] == '1'){
+        draw_chord(chordTime, chordNotation, i);
+      //}
+      //setTimeout("$(document).on('keypress', function(e){ match_event(String.fromCharCode(e.which) ,'"  
+     	//  + chordNotation + "');});", chordTime*1000 - 100);
 
-     	setTimeout("$(document).off('keypress')", chordTime*1000 + 100);
+     	//setTimeout("$(document).off('keypress')", chordTime*1000 + 100);
     }
 }
 
-function draw_chord(chordTime, chordNotation){
+function draw_chord(chordTime, chordNotation, index){
   
-  var topPosition = speed * chordTime + 55;
+  var topPosition =(speed * chordTime) + 55;
 
-  $('#chords').append("<div class='chord' style='top: " + topPosition + "px'>" + 
+  $('#chords').append("<div class='chord' id='chord_" + index + "' data-timing='"+chordTime+"' data-key='"+chordNotation+"' style='top: " + topPosition + "px'>" + 
     chordNotation.substring(0,1) + "</div>");
 }
 
 function startGame(event)
 {
+  //player.playVideo();am
+  //player.play();r(
+  //widget.play();
+  setupTimersForGame();
+  var player = soundcloud.getPlayer('sc-player');
+  player.api_play();
+  
 	console.log('starting game...');
+  //setTimeout("$('#chords').addClass('startgame')", 1000);
 	$('#chords').addClass('startgame');
-
-  player.playVideo();
+  
 }
