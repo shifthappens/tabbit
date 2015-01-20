0318e6eeced7800eff71a734bfebcb49611001f5
Henrique <lima.henrique@gmail.com>
2015-01-20 23:28:41 +0000

    animations for firefox

--- js/main.js
+++ js/main.js
@@ -18,24 +18,14 @@ $(document).ready(function() {
   $('#button-start').on('click', startGame);
 
    soundcloud.addEventListener('onPlayerReady', function(scPlayer, data) {
-      //scPlayer.api_play();
       $('#button-start').show();
    });
-  //SC.oEmbed("https://soundcloud.com/user723751824/get-lucky-daft-punk-feat", {color: "ff0066"},  document.getElementById("putTheWidgetHere"));
-  //console.log(player);
 
+   soundcloud.addEventListener('onMediaPlay', function(scPlayer, data) {
+      console.log("player started!!!");
+      $('#chords').addClass('startgame');
+   });
 });
-
-//function onYouTubeIframeAPIReady() {
-//  player = new YT.Player('player', {
-//    height: '250',
-//    width: '500',
-//    videoId: 'bBJ9RnbK8G4',
-//    events: {
-//      'onReady': onPlayerReady
-//    }
-//  });
-//}
 
 function onPlayerReady(event) {
   $("#button-start").show();
@@ -46,25 +36,16 @@ function start_song(){
     var lines = chordData.split(/\r\n|\n/);
     
     var lastChord = lines[lines.length-1].split(";");
-    //totalHeight = speed * lastChord[3 ];
-    //$('.cord').height(totalHeight);
-
+    
     for (var i=0; i<lines.length; i++){    	
-    	//clearTimeout(chordTimeout);
+    	
     	var chordParsed = lines[i].split(";");
      	var chordTime = chordParsed[2];
      	var chordNotation = chordParsed[1];
 
-      //setTimeout(function(){matchTime = true;}, chordTime*1000 -100);
-      //setTimeout(function(){matchTime = false;}, chordTime*1000 + 100);
-      
       //if(chordParsed[0] == '1'){
         draw_chord(chordTime, chordNotation, i);
       //}
-      //setTimeout("$(document).on('keypress', function(e){ match_event(String.fromCharCode(e.which) ,'"  
-     	//  + chordNotation + "');});", chordTime*1000 - 100);
-
-     	//setTimeout("$(document).off('keypress')", chordTime*1000 + 100);
     }
 }
 
@@ -78,17 +59,11 @@ function draw_chord(chordTime, chordNotation, index){
 
 function startGame(event)
 {
-  //player.playVideo();am
-  //player.play();r(
-  //widget.play();
   setupTimersForGame();
   var player = soundcloud.getPlayer('sc-player');
   player.api_play();
   
 	console.log('starting game...');
-  //setTimeout("$('#chords').addClass('startgame')", 1000);
-	$('#chords').addClass('startgame');
-  
 }
 
 function updateScore(points)
